099b161219fc8f7acb96df0e16d7019ae1520bd6
xiaohu1007 <1449394718@qq.com>
2024-06-05 00:49:32 +0800

    feat: 搭建three 实例

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,47 @@
 -->
 <template>
   <div>
-    <canvas id="three"></canvas>
+    <canvas ref="canvasRef"></canvas>
   </div>
 </template>
 
-<script setup lang="ts" name="home"></script>
+<script setup lang="ts" name="home">
+import * as THREE from 'three';
+
+// 获取画布
+const canvasRef = ref();
+
+onMounted(() => {
+  // 画布大小
+  const size = {
+    width: 800,
+    height: 600
+  }
+  
+  // 场景 Scene
+  const scene = new THREE.Scene();
+  
+  // 对象 Object
+  const geometry = new THREE.BoxGeometry(1, 1, 1); // 物体 Geometry
+  const material = new THREE.MeshBasicMaterial({ color: 'red', wireframe: true }); // 材质 material
+  const mesh = new THREE.Mesh(geometry, material); // 网络 mesh
+  scene.add(mesh); // 添加进场景
+  
+  // 相机 camera
+  const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+  camera.position.z = 3;
+  scene.add(camera);
+  
+  // 渲染器 renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvasRef.value
+  });
+  renderer.setSize(size.width, size.height);
+  renderer.render(scene, camera);
+
+  console.log(scene, canvasRef, '===>', canvasRef.value)
+});
+
+</script>
 
 <style scoped></style>
